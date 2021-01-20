d32018aa5ac01aa2297d5fceef6feec1b9060b47
yu.xiang <yu.xiang@alibaba-inc.com>
2021-01-20 23:11:29 +0800

    x

--- cms/wsgi.py
+++ cms/wsgi.py
@@ -5,6 +5,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+
+uwsgi --http :8001 --chdir /data/github/django-cms --module cms.wsgi
+
 """
 
 import os
